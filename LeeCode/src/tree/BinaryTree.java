32cd08ba7495b94073a345fab21bf52806268839
Ching-Tai <Ching-Tai@10.0.0.64>
2017-06-19 23:05:38 -0700

    commit

--- LeeCode/src/tree/BinaryTree.java
+++ LeeCode/src/tree/BinaryTree.java
@@ -145,4 +145,14 @@ public class BinaryTree {
 
 		return leftLca;
 	}
+	
+	/**
+	 * 
+	 * @param root
+	 * @return
+	 */
+    public int maxDepth(TreeNode root) {
+    	if (root == null) return 0;
+    	return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1; 
+    }
 }
