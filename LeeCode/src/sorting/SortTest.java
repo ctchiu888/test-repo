9b41d5bfd450b0775369e5f0c9e2608ea998d401
chichiu <chichiu@CHICHU-LAP1.oradev.oraclecorp.com>
2017-08-01 10:45:22 -0700

    push

--- LeeCode/src/sorting/SortTest.java
+++ LeeCode/src/sorting/SortTest.java
@@ -21,4 +21,13 @@ public class SortTest {
 		PrintUtil.printArray(nums2);
 	}
 
+	@Test
+	public void testMedianSlidingWindow() {
+		SearchSolution sol = new SearchSolution();
+		int[] nums0 = {};
+		int[] nums1 = { 1, 3, -1, -3, 5, 3, 6, 7, 9 };
+		int[] nums2 = { 1, 4, 2, 6, 7, 8, 3, 5 };
+		PrintUtil.printArray(sol.medianSlidingWindow(nums1, 3));
+	}
+
 }
